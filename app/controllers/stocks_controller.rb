da6a567d6f400bde0e236436caaea58956e5cf86
jaspinderkaur <jaspinder.kaur028@gmail.com>
2021-09-13 21:03:53 +0530

    added functionality for user to follow and unfollow stocks

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,19 +4,22 @@ class StocksController < ApplicationController
       @stock = Stock.new_lookup(params[:stock])
       if @stock
         respond_to do |format|
-          format.js {render partial: 'users/result'}
+          format.js { render partial: 'users/result' }
         end
       else
-        respond_to do |format|
-          flash.now[:alert] = "Please enter a valid symbol to search"
-          format.js {render partial: 'users/result'}
+        respond_to do |_format|
+          show_error_msg('Please enter a valid symbol to search')
         end
       end
     else
-      respond_to do |format|
-        flash.now[:alert] = "Please enter a symbol to search"
-        format.js {render partial: 'users/result'}
+      respond_to do |_format|
+        show_error_msg('Please enter a symbol to search')
       end
     end
+  end
+
+  def show_error_msg(message)
+    flash.now[:alert] = message
+    format.js { render partial: 'users/result' }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root to: 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
