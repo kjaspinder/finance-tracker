bd014ee89fdd144c8595c6bcd409fd7fdd7bb5cf
jaspinderkaur <jaspinder.kaur028@gmail.com>
2021-09-20 21:43:23 +0530

    added ui

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@
 
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
+  resources :friends, only: [:create, :destroy]
   devise_for :users
   root to: 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'friends', to: 'users#friends'
   get 'search_stock', to: 'stocks#search'
 end
