d9144ba487507f0f70df04c9368a05bb3f743171
jaspinderkaur <jaspinder.kaur028@gmail.com>
2021-10-05 14:58:27 +0400

    follow and unfollow friend, view profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@
 
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
-  resources :friends, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy]
   devise_for :users
   root to: 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'friends', to: 'users#friends'
   get 'search_stock', to: 'stocks#search'
   get 'search_friends', to: 'users#search'
+  resources :users, only: [:show]
 end
